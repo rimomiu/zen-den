85f4c6e115041ed5b2f1a71fdb8a9d85a4a08770
Silva Galstyan <silva.galstyan@gmail.com>
2024-06-03 19:15:11 +0000

    1st commit, SQL tables, migrations, organized files

---
+++ api/migrations/003_create_comments.py
@@ -0,0 +1,18 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE comments (
+            comment_id SERIAL PRIMARY KEY,
+            body VARCHAR(50) NOT NULL,
+            blog_id INT NOT NULL REFERENCES blogs (blog_id),
+            author INT NOT NULL REFERENCES users (user_id),
+            date_published DATE NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE comments;
+        """
+    ],
+]
---
+++ api/routers/blogs_router.py
@@ -0,0 +1,8 @@
+# from fastapi import APIRouter
+# from queries.blogs_queries import Blogs
+
+# router = APIRouter()
+
+# @router.post("/blogs")
+# def create_blogs(blog: Blogs):
+#     return blog
