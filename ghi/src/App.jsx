e845d1d0657f5250bbb91286e3c106271d3e74af
NekoBowl <159499381+rimomiu@users.noreply.github.com>
2024-06-18 13:04:41 -0500

    blog detail update

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,24 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './Nav'
+import AuthProvider from './components/AuthProvider' // Import AuthProvider
+
 import BlogList from './components/BlogList'
 import HomePage from './components/Home'
 import BlogDetail from './components/BlogDetail'
-// import Profile from './components/Profile'
+import Profile from './components/Profile'
 import SignInForm from './components/SignInForm'
 import SignUpForm from './components/SignUpForm'
 function App() {
     return (
-        <BrowserRouter>
-            <Nav />
-            <Routes>
-                <Route path="/blogs" element={<BlogList />} />
-                <Route path="/blogs/:blogId" element={<BlogDetail />} />
-                <Route path="/" element={<HomePage />} />
-                {/* <Route path="/user/:userId" element={<Profile />} /> */}
-                <Route path="/signup" element={<SignUpForm />} />
-                <Route path="/signin" element={<SignInForm />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Nav />
+                <Routes>
+                    <Route path="/blogs" element={<BlogList />} />
+                    <Route path="/blogs/:blogId" element={<BlogDetail />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/user/:userId" element={<Profile />} />
+                    <Route path="/signup" element={<SignUpForm />} />
+                    <Route path="/signin" element={<SignInForm />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     )
 }
 
--- ghi/src/components/Profile.jsx
+++ ghi/src/components/Profile.jsx
@@ -1,44 +1,106 @@
 // import { useState, useEffect } from 'react'
 // import { useParams } from 'react-router-dom'
+// import {
+//     Card,
+//     CardContent,
+//     Typography,
+//     Container,
+//     Grid,
+//     CircularProgress,
+// } from '@mui/material'
 
 // export default function Profile() {
-//     const [users, setUsers] = useState('')
+//     const [user, setUser] = useState([])
 //     const [blogs, setBlogs] = useState([])
-//     const { userId } = useParams('')
+//     const [comments, setComments] = useState([])
+//     const { userId } = useParams()
+//     const { blogId } = useParams()
+//     const { commentId } = useParams()
 
 //     const fetchData = async () => {
-//         const userUrl = `${import.meta.env.VITE_API_HOST}/users/${userId}`
-//         const response = await fetch(userUrl)
-//         if (response.ok) {
-//             const data = await response.json()
-//             setUsers(data.users)
-//             setBlogs(data.blogs)
+//         const userUrl = `${import.meta.env.VITE_API_HOST}/users/id/${userId}`
+//         const blogsUrl = `${import.meta.env.VITE_API_HOST}/blogs/${blogId}`
+//         const commentUrl = `${
+//             import.meta.env.VITE_API_HOST
+//         }/comments/${commentId}`
+
+//         const userResponse = await fetch(userUrl)
+//         const blogsResponse = await fetch(blogsUrl)
+//         const commentResponse = await fetch(commentUrl)
+
+//         if (userResponse.ok && blogsResponse.ok && commentResponse.ok) {
+//             const userData = await userResponse.json()
+//             const blogsData = await blogsResponse.json()
+//             const commentData = await commentResponse.json()
+
+//             setUser(userData)
+//             setBlogs(blogsData)
+//             setComments(commentsData)
 //         }
 //     }
 
 //     useEffect(() => {
 //         fetchData()
-//     }, [userId])
+//     }, [userId, blogId, commentId])
 
-//     if (!users) {
-//         return <div>Loading...</div>
+//     if (!user) {
+//         return <CircularProgress />
 //     }
 
 //     return (
-//         <div>
-//             <h2>User Profile</h2>
-//             <p>Username: {users.username}</p>
-//             <p>First Name: {users.firstName}</p>
-//             <p>Last Name: {users.lastName}</p>
-//             <h3>User Blogs</h3>
-//             <ul>
+//         <Container>
+//             <Typography variant="h2" component="h2" gutterBottom>
+//                 User Profile
+//             </Typography>
+//             <Card>
+//                 <CardContent>
+//                     <Typography variant="h6" component="p">
+//                         Username: {user.username}
+//                     </Typography>
+//                     <Typography variant="h6" component="p">
+//                         First Name: {user.firstName}
+//                     </Typography>
+//                     <Typography variant="h6" component="p">
+//                         Last Name: {user.lastName}
+//                     </Typography>
+//                 </CardContent>
+//             </Card>
+
+//             <Typography variant="h4" component="h4" gutterBottom>
+//                 Users' Blog
+//             </Typography>
+//             <Grid container spacing={2}>
 //                 {blogs.map((blog) => (
-//                     <li key={blog.id}>
-//                         <h4>{blog.title}</h4>
-//                         <p>{blog.content}</p>
-//                     </li>
+//                     <Grid item xs={12} sm={6} md={4} key={blog.id}>
+//                         <Card>
+//                             <CardContent>
+//                                 <Typography variant="h5" component="h4">
+//                                     {blog.title}
+//                                 </Typography>
+//                                 <Typography variant="body2" component="p">
+//                                     {blog.date_publish}
+//                                 </Typography>
+//                             </CardContent>
+//                         </Card>
+//                     </Grid>
 //                 ))}
-//             </ul>
-//         </div>
+//             </Grid>
+//             <Typography variant="h4" component="h4" gutterBottom>
+//                 Users' Comments
+//             </Typography>
+//             <Grid container spacing={2}>
+//                 {comments.map((comment) => (
+//                     <Grid item xs={12} sm={6} md={4} key={comment.id}>
+//                         <Card>
+//                             <CardContent>
+//                                 <Typography variant="h5" component="h4">
+//                                     {comment.body}
+//                                 </Typography>
+//                             </CardContent>
+//                         </Card>
+//                     </Grid>
+//                 ))}
+//             </Grid>
+//         </Container>
 //     )
 // }
