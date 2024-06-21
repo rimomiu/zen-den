5dc70620b74b257151f97da4827408e3e504e10a
Silva <silva.galstyan@gmail.com>
2024-06-20 17:14:40 -0700

    updated backend endpoint auth code and migrations to fix some minor bugs

--- api/migrations/002_create_blogs.py
+++ api/migrations/002_create_blogs.py
@@ -7,7 +7,7 @@ steps = [
         title VARCHAR(100) NOT NULL,
         pic_url VARCHAR(100) NOT NULL,
         content VARCHAR(1500) NOT NULL,
-        author_id SMALLINT NOT NULL REFERENCES users (user_id),
+        author_id SMALLINT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
         date_published DATE NOT NULL
         );
         """,
