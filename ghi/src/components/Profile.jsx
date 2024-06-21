0f1eef511c7916573cb2d7ad0fe07be6d8d0d4a2
Jihanyu Miao <miaojihanyu@gmail.com>
2024-06-21 01:50:25 +0000

    Profile page& auth blog detail page done

--- ghi/src/components/Profile.jsx
+++ ghi/src/components/Profile.jsx
@@ -1,106 +1,179 @@
-// import { useState, useEffect } from 'react'
-// import { useParams } from 'react-router-dom'
-// import {
-//     Card,
-//     CardContent,
-//     Typography,
-//     Container,
-//     Grid,
-//     CircularProgress,
-// } from '@mui/material'
+import { useState, useEffect, useCallback } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import {
+    Card,
+    CardContent,
+    Typography,
+    Container,
+    Grid,
+    CircularProgress,
+    CardActionArea,
+    Button,
+} from '@mui/material'
 
-// export default function Profile() {
-//     const [user, setUser] = useState([])
-//     const [blogs, setBlogs] = useState([])
-//     const [comments, setComments] = useState([])
-//     const { userId } = useParams()
-//     const { blogId } = useParams()
-//     const { commentId } = useParams()
+import { signout } from '../services/authService'
 
-//     const fetchData = async () => {
-//         const userUrl = `${import.meta.env.VITE_API_HOST}/users/id/${userId}`
-//         const blogsUrl = `${import.meta.env.VITE_API_HOST}/blogs/${blogId}`
-//         const commentUrl = `${
-//             import.meta.env.VITE_API_HOST
-//         }/comments/${commentId}`
+export default function Profile() {
+    const [user, setUser] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [comments, setComments] = useState([])
+    const { userId } = useParams()
+    const navigate = useNavigate()
 
-//         const userResponse = await fetch(userUrl)
-//         const blogsResponse = await fetch(blogsUrl)
-//         const commentResponse = await fetch(commentUrl)
+    const fetchData = useCallback(async () => {
+        const userUrl = `${import.meta.env.VITE_API_HOST}/users/id/${userId}`
+        const blogsUrl = `${
+            import.meta.env.VITE_API_HOST
+        }/users/id/${userId}/blogs`
+        const commentUrl = `${
+            import.meta.env.VITE_API_HOST
+        }/comments/users/${userId}`
 
-//         if (userResponse.ok && blogsResponse.ok && commentResponse.ok) {
-//             const userData = await userResponse.json()
-//             const blogsData = await blogsResponse.json()
-//             const commentData = await commentResponse.json()
+        const userResponse = await fetch(userUrl)
+        const blogsResponse = await fetch(blogsUrl)
+        const commentResponse = await fetch(commentUrl)
 
-//             setUser(userData)
-//             setBlogs(blogsData)
-//             setComments(commentsData)
-//         }
-//     }
+        if (userResponse.ok && blogsResponse.ok) {
+            const userData = await userResponse.json()
+            const blogsData = await blogsResponse.json()
 
-//     useEffect(() => {
-//         fetchData()
-//     })
+            setUser(userData)
+            setBlogs(blogsData)
+        }
+        if (commentResponse.ok) {
+            const commentsData = await commentResponse.json()
+            setComments(commentsData)
+        }
+    }, [userId])
 
-//     if (!user) {
-//         return <CircularProgress />
-//     }
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
 
-//     return (
-//         <Container>
-//             <Typography variant="h2" component="h2" gutterBottom>
-//                 User Profile
-//             </Typography>
-//             <Card>
-//                 <CardContent>
-//                     <Typography variant="h6" component="p">
-//                         Username: {user.username}
-//                     </Typography>
-//                     <Typography variant="h6" component="p">
-//                         First Name: {user.firstName}
-//                     </Typography>
-//                     <Typography variant="h6" component="p">
-//                         Last Name: {user.lastName}
-//                     </Typography>
-//                 </CardContent>
-//             </Card>
+    const handleDeleteBlog = async (blogId) => {
+        const deleteUrl = `${import.meta.env.VITE_API_HOST}/blogs/${blogId}`
+        const response = await fetch(deleteUrl, {
+            method: 'DELETE',
+            credentials: 'include',
+        })
+        if (response.ok) {
+            fetchData()
+        }
+    }
 
-//             <Typography variant="h4" component="h4" gutterBottom>
-//                 Users' Blog
-//             </Typography>
-//             <Grid container spacing={2}>
-//                 {blogs.map((blog) => (
-//                     <Grid item xs={12} sm={6} md={4} key={blog.id}>
-//                         <Card>
-//                             <CardContent>
-//                                 <Typography variant="h5" component="h4">
-//                                     {blog.title}
-//                                 </Typography>
-//                                 <Typography variant="body2" component="p">
-//                                     {blog.date_publish}
-//                                 </Typography>
-//                             </CardContent>
-//                         </Card>
-//                     </Grid>
-//                 ))}
-//             </Grid>
-//             <Typography variant="h4" component="h4" gutterBottom>
-//                 Users' Comments
-//             </Typography>
-//             <Grid container spacing={2}>
-//                 {comments.map((comment) => (
-//                     <Grid item xs={12} sm={6} md={4} key={comment.id}>
-//                         <Card>
-//                             <CardContent>
-//                                 <Typography variant="h5" component="h4">
-//                                     {comment.body}
-//                                 </Typography>
-//                             </CardContent>
-//                         </Card>
-//                     </Grid>
-//                 ))}
-//             </Grid>
-//         </Container>
-//     )
-// }
+    const handleDeleteComment = async (commentId) => {
+        const deleteUrl = `${
+            import.meta.env.VITE_API_HOST
+        }/comments/${commentId}`
+        const response = await fetch(deleteUrl, {
+            method: 'DELETE',
+            credentials: 'include',
+        })
+        if (response.ok) {
+            setComments(comments.filter((comment) => comment.id !== commentId))
+        }
+    }
+
+    const handleLogout = () => {
+        signout()
+        navigate('/')
+    }
+
+    if (!user) {
+        return <CircularProgress />
+    }
+
+    return (
+        <Container>
+            <Typography variant="h2" component="h2" gutterBottom>
+                User Profile
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleLogout}>
+                Logout
+            </Button>
+            <Card>
+                <CardContent>
+                    <Typography variant="h6" component="p">
+                        Username: {user.username}
+                    </Typography>
+                    <Typography variant="h6" component="p">
+                        First Name: {user.first_name}
+                    </Typography>
+                    <Typography variant="h6" component="p">
+                        Last Name: {user.last_name}
+                    </Typography>
+                </CardContent>
+            </Card>
+
+            <Typography variant="h4" component="h4" gutterBottom>
+                User Blog
+            </Typography>
+            <Grid container spacing={2}>
+                {blogs.map((blog) => {
+                    return (
+                        <Grid item xs={12} sm={6} md={4} key={blog.blog_id}>
+                            <Card>
+                                <CardActionArea
+                                    component={Link}
+                                    to={`/blogs/${blog.blog_id}`}
+                                >
+                                    <CardContent>
+                                        <Typography variant="h5" component="h4">
+                                            {blog.title}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            component="p"
+                                        >
+                                            {blog.date_publish}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardContent>
+                                    <Button
+                                        variant="contained"
+                                        color="error"
+                                        onClick={() =>
+                                            handleDeleteBlog(blog.blog_id)
+                                        }
+                                        size="small"
+                                    >
+                                        Delete Blog
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+            <Typography variant="h4" component="h4" gutterBottom>
+                User Comments
+            </Typography>
+            <Grid container spacing={2}>
+                {comments.map((comment) => (
+                    <Grid item xs={12} sm={6} md={4} key={comment.comment_id}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="h4">
+                                    {comment.body}
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <Button
+                                    variant="contained"
+                                    color="error"
+                                    onClick={() =>
+                                        handleDeleteComment(comment.comment_id)
+                                    }
+                                    size="small"
+                                >
+                                    Delete Comment
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    )
+}
